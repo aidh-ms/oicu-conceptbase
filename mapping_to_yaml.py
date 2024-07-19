4fc96093acecb4f3ceeb0d11ecb135263fed5a8e
Christian Porschen <christian.porschen@gmail.com>
2024-07-19 11:26:53 +0200

    update concepts

--- mapping_to_yaml.py
+++ mapping_to_yaml.py
@@ -1,31 +1,35 @@
 import csv
 import yaml
 import os
+from copy import deepcopy
 
-
-with open('concepts/HeartRate.yml', 'r') as file:
+with open('example.yml', 'r') as file:
     yaml_template = yaml.safe_load(file)
 
 
 def create_yaml_from_template(data, template, output_dir):
-    new_yaml = template.copy()
+    # Use deepcopy to ensure the template is not modified
+    new_yaml = deepcopy(template)
+    print(data['Concept Name'])
+    # Directly update the necessary fields
     new_yaml['name'] = data['Concept Name']
     new_yaml['description'] = data['Description']
     new_yaml['unit'] = data['Standard Unit']
+    new_yaml['upper_limit'] = data['Absolute Upper']
+    new_yaml['lower_limit'] = data['Absolute Lower']
     new_yaml['identifiers']['snomed'] = data['SCT CONEPT ID']
     new_yaml['identifiers']['loinc'] = data['LOINC Code']
-
-    # Update mapper information
     new_yaml['mapper'][0]['source'] = 'mimiciv'
     new_yaml['mapper'][0]['unit'] = data['MIMIC Unit']
     new_yaml['mapper'][0]['params']['schema'] = data['MIMICIV schema']
     new_yaml['mapper'][0]['params']['table'] = data['MIMICIV table']
     new_yaml['mapper'][0]['params']['constraints']['itemid'] = data['MIMICIV IDs']
 
-    # Write to a new YAML file
+    # Write the updated YAML to a new file
     output_file = os.path.join(output_dir, f"{data['Concept Name']}.yml")
     with open(output_file, 'w') as outfile:
-        yaml.dump(new_yaml, outfile, default_flow_style=False)
+        yaml.dump(new_yaml, outfile, allow_unicode=True,
+                  default_flow_style=False)
 
 
 # Read the CSV file and process flagged rows
